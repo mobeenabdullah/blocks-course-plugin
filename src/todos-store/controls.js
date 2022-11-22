ba86399b5775a8114b88f4d878401eb873443eee
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-11-22 20:10:22 +0500

    Displaying the Todos in the Todo List Block + Create Todo Work

--- src/todos-store/controls.js
+++ src/todos-store/controls.js
@@ -1,4 +1,4 @@
-import { FETCH_TODOS } from './types';
+import { FETCH_TODOS, CREATE_TODO } from './types';
 
 export const fetchTodos = () => {
 	return {
@@ -6,15 +6,43 @@ export const fetchTodos = () => {
 	};
 };
 
+export const createTodo = (title) => {
+	return {
+		type: CREATE_TODO,
+		title,
+	};
+};
+
 export default {
 	FETCH_TODOS() {
 		return fetch(
-			'https://jsonplacehaolder.typicode.com/todos?_limit=10'
+			'https://jsonplaceholder.typicode.com/todos?_limit=10'
 		).then((response) => {
 			if (response.ok) {
 				return response.json();
 			}
 			throw new Error('Could not fetch todos');
 		});
+	},
+
+	CREATE_TODO({ title }) {
+		return window
+			.fetch('https://jsonplaceholder.typicode.com/todos', {
+				method: 'POST',
+				body: JSON.stringify({
+					title,
+					completed: false,
+					userId: 1,
+				}),
+				headers: {
+					'Content-type': 'application/json; charset=UTF-8',
+				},
+			})
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				}
+				throw new Error('Could not create todo.');
+			});
 	},
 };
